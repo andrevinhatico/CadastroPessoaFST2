ff5beefea09400936e792c41cf447e3816670f0e
Andre Ribeiro <andrebahia7@gmail.com>
2022-04-11 20:56:36 -0300

    Encontro Remoto 4

--- Classes/PessoaJuridica.cs
+++ Classes/PessoaJuridica.cs
@@ -1,10 +1,11 @@
+using System.Text.RegularExpressions;
 using CadastroPessoaFST2.Interfaces;
 
 namespace CadastroPessoaFST2.Classes
 {
     public class PessoaJuridica : Pessoa , IPessoaJuridica
     {
-        public string? Cnpj { get; set; }
+        public string? cnpj { get; set; }
 
         public string? razaoSocial { get; set; }
 
@@ -12,10 +13,30 @@ namespace CadastroPessoaFST2.Classes
         {
             throw new NotImplementedException();
         }
-
+        //XX.XXX.XXX/0001-XX --- XXXXXXXX0001XX
         public bool ValidarCNPJ(string cnpj)
         {
-            throw new NotImplementedException();
+            
+            if (Regex.IsMatch(cnpj, @"(^(\d{2}.\d{3}.\d{3}/\d{4}-\d{2})|(\d{14})$)"))
+            {
+                if (cnpj.Length == 18)
+                {
+                    if (cnpj.Substring(11, 4) == "0001")
+                    {
+                        return true;
+                    }
+                    
+                } else if (cnpj.Length == 14)
+
+                {
+                    if (cnpj.Substring(8, 4)== "0001")
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,35 +1,69 @@
-﻿using CadastroPessoaFST2.Classes;
+﻿ using CadastroPessoaFST2.Classes;
 
-PessoaFisica novaPf = new PessoaFisica();
-PessoaFisica metodoPf = new PessoaFisica();
-Endereco novoEnd = new Endereco();
+PessoaJuridica metodopj = new PessoaJuridica();
+PessoaJuridica novaPj = new PessoaJuridica();
+Endereco endPj = new Endereco();
 
-novaPf.nome = "Andre";
-novaPf.dataNasc = new DateTime (2000,01,01);
-novaPf.cpf = "1234567890";
-novaPf.rendimento = 10000.5f;
+novaPj.nome = "Nome Pj";
+novaPj.razaoSocial = "Razao Social PJ";
+novaPj.cnpj = "00.000.000/0001-00";
+novaPj.rendimento = 10000.5f;
 
-novoEnd.logradouro = "Avenida Bora Baea";
-novoEnd.numero = 5988;
-novoEnd.complemento =  "Cidade Tricolor";
-novoEnd.endComercial = true;
+endPj.logradouro = "Avenida PJ";
+endPj.numero = 218;
+endPj.complemento = "Rua Nova";
+endPj.endComercial = true;
 
-novaPf.endereco = novoEnd;
-
-Console.WriteLine($"Nome: {novaPf.nome}");
-Console.WriteLine($"Data de Nascimento: {novaPf.dataNasc}");
-Console.WriteLine($"CPF: {novaPf.cpf}");
-Console.WriteLine($"Endereço: {novaPf.endereco.logradouro}, {novaPf.endereco.numero}, {novaPf.endereco.complemento}");
+novaPj.endereco = endPj;
 
 Console.WriteLine(@$"
-Nome: {novaPf.nome}
-Data de Nascimento: {novaPf.dataNasc}
-CPF: {novaPf.cpf}
-Endereço: {novaPf.endereco.logradouro}, {novaPf.endereco.numero}, {novaPf.endereco.complemento}
+Nome: {novaPj.nome}
+Razão Social: {novaPj.razaoSocial}
+CNPJ: {novaPj.cnpj}
+CNPJ Válido: {metodopj.ValidarCNPJ(novaPj.cnpj)}
+Endereço: {novaPj.endereco.logradouro}, {novaPj.endereco.numero}, {novaPj.endereco.complemento}
 ");
 
 
-Console.WriteLine($"{metodoPf.ValidarDataNasc(novaPf.dataNasc)}");
+// Console.WriteLine(metodopj.ValidarCNPJ("00.000.000/0001-00"));
+
+
+
+
+
+
+
+
+// PessoaFisica novaPf = new PessoaFisica();
+// PessoaFisica metodoPf = new PessoaFisica();
+// Endereco novoEnd = new Endereco();
+
+// novaPf.nome = "Andre";
+// novaPf.dataNasc = new DateTime (2000,01,01);
+// novaPf.cpf = "1234567890";
+// novaPf.rendimento = 10000.5f;
+
+// novoEnd.logradouro = "Avenida Bora Baea";
+// novoEnd.numero = 5988;
+// novoEnd.complemento =  "Cidade Tricolor";
+// novoEnd.endComercial = true;
+
+// novaPf.endereco = novoEnd;
+
+// Console.WriteLine($"Nome: {novaPf.nome}");
+// Console.WriteLine($"Data de Nascimento: {novaPf.dataNasc}");
+// Console.WriteLine($"CPF: {novaPf.cpf}");
+// Console.WriteLine($"Endereço: {novaPf.endereco.logradouro}, {novaPf.endereco.numero}, {novaPf.endereco.complemento}");
+
+// Console.WriteLine(@$"
+// Nome: {novaPf.nome}
+// Data de Nascimento: {novaPf.dataNasc}
+// CPF: {novaPf.cpf}
+// Endereço: {novaPf.endereco.logradouro}, {novaPf.endereco.numero}, {novaPf.endereco.complemento}
+// ");
+
+
+// Console.WriteLine($"{metodoPf.ValidarDataNasc(novaPf.dataNasc)}");
 
 
 
